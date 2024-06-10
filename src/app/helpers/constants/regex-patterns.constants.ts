91465c6d2cf21900b2939e8368230584751c138c
sarahBortoloti <sarah.bortoloti.15@gmail.com>
2024-06-10 19:01:16 -0300

    fix: adiciona escape para permitir que o hiffen seja aceito como caractere especial

--- src/app/helpers/constants/regex-patterns.constants.ts
+++ src/app/helpers/constants/regex-patterns.constants.ts
@@ -1,8 +1,8 @@
 export const numbersCharsRegex = /[0-9]/;
 export const upperLettersRegex = /[A-Z]/;
 export const lowerLettersRegex = /[a-z]/;
-export const specialCharsRegex = /[!@#$%^&*()-+]/;
+export const specialCharsRegex = /[!@#$%^&*()\-\+]/;
 
-export const forbiddenPassword = /[^0-9A-Za-z!@#$%^&*()-+]/g;
+export const forbiddenPassword = /[^0-9A-Za-z!@#$%^&*()\-\+]/g;
 
 export const repeatedCharsRegex = /(.).*?\1/;
